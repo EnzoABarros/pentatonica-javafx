04409596d0ee41fa898ece0a495da43edd304e54
Vinicius Soares <vinicius.soaressilva06@gmail.com>
2025-06-10 21:45:40 -0300

    Adiciona funcionalidade de criar pagamento

--- src/main/java/br/pentatonica/guitarrascrud/pagamentos/Pagamento.java
+++ src/main/java/br/pentatonica/guitarrascrud/pagamentos/Pagamento.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 public class Pagamento implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String titulo;
     private double preco;
     private String email;
@@ -12,43 +14,29 @@ public class Pagamento implements Serializable {
 
     public Pagamento() {}
 
-    public String getTitulo() {
-        return titulo;
-    }
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco; }
 
-    public double getPreco() {
-        return preco;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 
-    public String getEmail() {
-        return email;
-    }
+    public LocalDateTime getData() { return data; }
+    public void setData(LocalDateTime data) { this.data = data; }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getData() {
-        return data;
-    }
-
-    public void setData(LocalDateTime data) {
-        this.data = data;
+    @Override
+    public String toString() {
+        return "Pagamento{" +
+                "titulo='" + titulo + '\'' +
+                ", preco=" + preco +
+                ", email='" + email + '\'' +
+                ", status='" + status + '\'' +
+                ", data=" + data +
+                '}';
     }
 }
